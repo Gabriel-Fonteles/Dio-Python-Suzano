407e101a6a1e30fd89de747770ed1cbf0d7901c0
Gabriel-Fonteles <bielfonteles3@gmail.com>
2025-05-30 10:33:41 -0300

    Corrige submódulo em 'tipos de operadores' e adiciona como pasta comum

---
+++ Sintaxe-Basica-com-Python/tipos de operadores/operadores_associacao.py
@@ -0,0 +1,6 @@
+frutas = ["limão", "uva", "Laranja"]
+curso = "Curos de Python"
+
+print("laranja" in frutas)
+print("limão" not in frutas)
+print("Python" in curso)
---
+++ Sintaxe-Basica-com-Python/tipos de operadores/operadores_atibuicao.py
@@ -0,0 +1,21 @@
+
+saldo = 200
+
+saldo += 10
+print(saldo)
+
+saldo -= 5
+print(saldo)
+
+saldo //= 2
+print(saldo)
+
+saldo *= 10
+print(saldo)
+
+saldo %= 4
+print(saldo)
+
+saldo **= 2
+print(saldo)
+
